9884b3aea8f859b9a5cdf7c1764f919151a22ba2
Junedale <gayetajunedale0@gmail.com>
2023-01-09 22:11:58 +0800

    Initial commit. Initial implementation of evaluating unary expression in VisLang

---
+++ src/core/ast/UnaryExpr.java
@@ -0,0 +1,34 @@
+package core.ast;
+
+import core.antlr4.VisLangParser.UnaryNotContext;
+import core.antlr4.VisLangParser.UnaryMinContext;
+import core.interpreter.Evaluator;
+import core.interpreter.Value;
+
+public class UnaryExpr {
+
+    private final Evaluator eval;
+
+    public UnaryExpr(Evaluator eval) {
+        this.eval = eval;
+    }
+
+    public Value evaluate(UnaryNotContext ctx) {
+        Value right = eval.visit(ctx.expr());
+
+        if(right.isBoolean())
+            return new Value(!right.toBool());
+        throw new RuntimeException(ctx.Not().getText() + " cannot be applied to non boolean type");
+    }
+
+    public Value evaluate(UnaryMinContext ctx) {
+        Value right = eval.visit(ctx);
+
+        if(right.isInteger())
+            return new Value(-right.toInt());
+        else if(right.isDouble())
+            return new Value(-right.toDouble());
+        else
+            throw new RuntimeException(ctx.Sub().getText() + " cannot be applied to non number type");
+    }
+}
