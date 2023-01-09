fe7638aa252502f6d74a35825bc08b2371403132
Junedale <gayetajunedale0@gmail.com>
2023-01-09 22:07:27 +0800

    Implemented logical expression

--- src/core/interpreter/Evaluator.java
+++ src/core/interpreter/Evaluator.java
@@ -4,15 +4,18 @@ import core.antlr4.VisLangBaseVisitor;
 import core.antlr4.VisLangParser;
 import core.ast.ConditionalExpr;
 import core.ast.Literals;
+import core.ast.LogicalExpr;
 
 public class Evaluator extends VisLangBaseVisitor<Value> {
 
     private final Literals literal;
     private final ConditionalExpr comparison;
+    private final LogicalExpr logic;
 
     public Evaluator() {
         literal    = new Literals();
         comparison = new ConditionalExpr(this);
+        logic      = new LogicalExpr(this);
     }
 
     @Override
@@ -43,5 +46,15 @@ public class Evaluator extends VisLangBaseVisitor<Value> {
     @Override
     public Value visitComparison(VisLangParser.ComparisonContext ctx) {
         return comparison.evaluate(ctx);
+    }
+
+    @Override
+    public Value visitLogicalAnd(VisLangParser.LogicalAndContext ctx) {
+        return logic.evaluate(ctx);
+    }
+
+    @Override
+    public Value visitLogicalOr(VisLangParser.LogicalOrContext ctx) {
+        return logic.evaluate(ctx);
     }
 }
