2713c660ecdf6c85479a73a3950a206b56fb8a64
Junedale <gayetajunedale0@gmail.com>
2023-01-09 22:08:01 +0800

    Initial commit. Initial implementation of evaluating logical expression in VisLang

---
+++ src/core/ast/LogicalExpr.java
@@ -0,0 +1,43 @@
+package core.ast;
+
+import core.antlr4.VisLangParser.LogicalAndContext;
+import core.antlr4.VisLangParser.LogicalOrContext;
+import core.interpreter.Evaluator;
+import core.interpreter.Value;
+
+public class LogicalExpr {
+
+    private final Evaluator eval;
+
+    public LogicalExpr(Evaluator eval) {
+        this.eval = eval;
+    }
+
+    public Value evaluate(LogicalAndContext ctx) {
+        Value left  = eval.visit(ctx.expr(0));
+        Value right = eval.visit(ctx.expr(1));
+
+        if(left == null || right == null) {
+            throw new RuntimeException("Expression contains null value");
+        } else if(left.isBoolean() && right.isBoolean()) {
+            return new Value(left.toBool() && right.toBool());
+        } else {
+            throw new RuntimeException("");
+        }
+
+    }
+
+    public Value evaluate(LogicalOrContext ctx) {
+        Value left  = eval.visit(ctx.expr(0));
+        Value right = eval.visit(ctx.expr(1));
+
+        if(left == null || right == null) {
+            throw new RuntimeException("Expression contains null value");
+        } else if(left.isBoolean() || right.isBoolean()) {
+            return new Value(left.toBool() && right.toBool());
+        } else {
+            throw new RuntimeException("");
+        }
+
+    }
+}
