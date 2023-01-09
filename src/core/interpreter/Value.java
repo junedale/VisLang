634369756350ca598ce3c2455448e01226bee012
Junedale <gayetajunedale0@gmail.com>
2023-01-09 20:38:49 +0800

    Initial commit. Initial implementation

---
+++ src/core/interpreter/Value.java
@@ -0,0 +1,90 @@
+package core.interpreter;
+
+public class Value {
+    public static final Value NULL = new Value();
+    public static final Value VOID = new Value();
+
+    private Object value;
+
+    private Value() {
+        value = new Object();
+    }
+
+    public Value(String val) {
+        value = val;
+    }
+
+    public Value(Integer val) {
+        value = val;
+    }
+
+    public Value(Double val) {
+        value = val;
+    }
+
+    public Value(Boolean val) {
+        value = val;
+
+    }
+
+    public Integer toInt() {
+        return (Integer) value;
+    }
+
+    public Double toDouble() {
+        return (Double) value;
+    }
+
+    public String toStr() {
+        return String.valueOf(value);
+    }
+
+    public Boolean toBool() {
+        return (Boolean) value;
+    }
+
+    public Boolean isString() {
+        return value instanceof String;
+    }
+
+    public Boolean isInteger() {
+        return value instanceof Integer;
+    }
+
+    public Boolean isDouble() {
+        return value instanceof Double;
+    }
+
+    public Boolean isBoolean() {
+        return value instanceof Boolean;
+    }
+
+    public Boolean isVoid() {
+        return value == VOID;
+    }
+
+    public Boolean isNull() {
+        return value == NULL;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(value == obj)
+            return true;
+
+        if(!(obj instanceof Value))
+            return false;
+
+        return value.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return isNull() ? "NULL" : isVoid() ? "VOID" : String.valueOf(value);
+    }
+}
