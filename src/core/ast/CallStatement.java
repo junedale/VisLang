f81bb9666fd3fc24b562585ab642b9a09d5baf64
Junedale <gayetajunedale0@gmail.com>
2023-01-10 19:25:18 +0800

    Initial commit. Initial implementation of evaluating call statement

---
+++ src/core/ast/CallStatement.java
@@ -0,0 +1,35 @@
+package core.ast;
+
+import core.antlr4.VisLangParser.PrintContext;
+import core.antlr4.VisLangParser.PrintlnContext;
+import core.interpreter.Evaluator;
+import core.interpreter.Value;
+
+public class CallStatement {
+
+    private final Evaluator eval;
+
+    public CallStatement(Evaluator eval) {
+        this.eval = eval;
+    }
+
+    public Value evaluate(PrintContext ctx) {
+        Value val = eval.visit(ctx.expr());
+        if(val == null) {
+            throw new RuntimeException("Cannot resolve " + ctx.getText());
+        } else {
+            System.out.print(eval.visit(ctx.expr()).toString());
+        }
+        return Value.VOID;
+    }
+
+    public Value evaluate(PrintlnContext ctx) {
+        Value val = eval.visit(ctx.expr());
+        if(val == null) {
+            System.out.println();
+        } else {
+            System.out.println(eval.visit(ctx.expr()).toString());
+        }
+        return Value.VOID;
+    }
+}
