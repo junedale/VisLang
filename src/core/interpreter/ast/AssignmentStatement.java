39b4986936436a1549b45fe5b9e2c8d2b4ddefe0
Junedale <gayetajunedale0@gmail.com>
2023-01-11 18:12:41 +0800

    removed implementation of assign action context

--- src/core/interpreter/ast/AssignmentStatement.java
+++ src/core/interpreter/ast/AssignmentStatement.java
@@ -1,9 +1,7 @@
 package core.interpreter.ast;
 
-import core.antlr4.VisLangLexer;
 import core.antlr4.VisLangParser.IdentifierContext;
 import core.antlr4.VisLangParser.AssignmentContext;
-import core.antlr4.VisLangParser.AssignActionContext;
 import core.interpreter.Evaluator;
 import core.interpreter.ScopeResolver;
 import core.interpreter.Value;
@@ -28,93 +26,6 @@ public class AssignmentStatement {
         return Value.VOID;
     }
 
-    public Value evaluate(AssignActionContext ctx) {
-        String identifier  = ctx.Identifier().getText();
-        Value left         = eval.visit(ctx.Identifier());
-        Value right        = eval.visit(ctx.expr());
-
-        if(left == null || right == null) {
-            throw new RuntimeException("Incomplete expression: " + ctx.getText());
-        }
-
-        if((left.isInteger() || left.isDouble()) && (right.isInteger() || right.isDouble())) {
-            return switch(ctx.op.getType()) {
-                case VisLangLexer.Add_Assign -> addAssign(identifier, left, right);
-                case VisLangLexer.Sub_Assign -> subAssign(identifier, left, right);
-                case VisLangLexer.Mul_Assign -> mulAssign(identifier, left, right);
-                case VisLangLexer.Div_Assign -> divAssign(identifier, left, right);
-                case VisLangLexer.Mod_Assign -> modAssign(identifier, left, right);
-                default -> throw new RuntimeException("Unknown operator: " + ctx.op.getText());
-            };
-        } else {
-            throw new RuntimeException("cannot use " + ctx.op.getText() + " in non number type");
-        }
-    }
-
-    private Value modAssign(String identifier, Value left, Value right) {
-        if(left.isInteger() && right.isInteger()) {
-            scope.reAssign(identifier, new Value(left.toInt() % right.toInt()));
-            return Value.VOID;
-        } else {
-            throw  new RuntimeException("Incompatible type: ");
-        }
-    }
-
-    private Value divAssign(String identifier, Value left, Value right) {
-        if(left.isInteger() && right.isInteger()) {
-            scope.reAssign(identifier, new Value(left.toInt() / right.toInt()));
-            return Value.VOID;
-        } else if(left.isDouble() && right.isDouble()) {
-            scope.reAssign(identifier, new Value(left.toInt() / right.toInt()));
-            return Value.VOID;
-        } else if((left.isInteger() && right.isDouble()) || (left.isDouble() && right.isDouble())) {
-            scope.reAssign(identifier, new Value(left.toDouble() / left.toDouble()));
-        }
-        return Value.VOID;
-    }
-
-    private Value mulAssign(String identifier, Value left, Value right) {
-        if(left.isInteger() && right.isInteger()) {
-            scope.reAssign(identifier, new Value(left.toInt() * right.toInt()));
-            return Value.VOID;
-        } else if(left.isDouble() && right.isDouble()) {
-            scope.reAssign(identifier, new Value(left.toInt() * right.toInt()));
-            return Value.VOID;
-        } else if((left.isInteger() && right.isDouble()) || (left.isDouble() && right.isDouble())) {
-            scope.reAssign(identifier, new Value(left.toDouble() * left.toDouble()));
-        }
-        return Value.VOID;
-    }
-
-    private Value subAssign(String identifier, Value left, Value right) {
-        if(left.isInteger() && right.isInteger()) {
-            scope.reAssign(identifier, new Value(left.toInt() - right.toInt()));
-            return Value.VOID;
-        } else if(left.isDouble() && right.isDouble()) {
-            scope.reAssign(identifier, new Value(left.toInt() - right.toInt()));
-            return Value.VOID;
-        } else if((left.isInteger() && right.isDouble()) || (left.isDouble() && right.isDouble())) {
-            scope.reAssign(identifier, new Value(left.toDouble() - left.toDouble()));
-        } else {
-            throw new RuntimeException("Incompatible type ");
-        }
-        return Value.VOID;
-    }
-
-    private Value addAssign(String identifier, Value left, Value right) {
-        if(left.isInteger() && right.isInteger()) {
-            scope.reAssign(identifier, new Value(left.toInt() + right.toInt()));
-            return Value.VOID;
-        } else if(left.isDouble() && right.isDouble()) {
-            scope.reAssign(identifier, new Value(left.toInt() + right.toInt()));
-            return Value.VOID;
-        } else if((left.isInteger() && right.isDouble()) || (left.isDouble() && right.isDouble())) {
-            scope.reAssign(identifier, new Value(left.toDouble() + left.toDouble()));
-        } else {
-            throw new RuntimeException("Incompatible type ");
-        }
-        return Value.VOID;
-    }
 
     public void setScope(ScopeResolver scope) {
         this.scope = scope;
