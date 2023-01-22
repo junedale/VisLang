0769259acbbfd5e680755670ae802cadd29482b6
Junedale <gayetajunedale0@gmail.com>
2023-01-22 19:36:59 +0800

    implemented functions and function related methods

--- src/core/interpreter/ast/CallStatement.java
+++ src/core/interpreter/ast/CallStatement.java
@@ -1,9 +1,16 @@
 package core.interpreter.ast;
 
+import core.interpreter.Function;
 import core.interpreter.parser.VisLangParser.PrintContext;
 import core.interpreter.parser.VisLangParser.PrintlnContext;
+import core.interpreter.parser.VisLangParser.FunCallContext;
+import core.interpreter.parser.VisLangParser.ExprContext;
+import core.interpreter.parser.VisLangParser.FunCallExprContext;
 import core.interpreter.Evaluator;
 import core.interpreter.Value;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CallStatement {
 
@@ -31,5 +38,28 @@ public class CallStatement {
             System.out.println(val);
         }
         return Value.VOID;
+    }
+
+    public Value evaluate(FunCallContext ctx) {
+        List<ExprContext> parameters = ctx.params() != null ?
+                ctx.params().expr() : new ArrayList<>();
+        String identifier = ctx.Identifier().getText() + parameters.size();
+
+        Function function = eval.getFun().get(identifier);
+        if(function != null) {
+            List<Value> arguments = new ArrayList<>(parameters.size());
+
+            for(ExprContext param : parameters) {
+                arguments.add(eval.visit(param));
+            }
+
+            return function.invoke(arguments, eval.getFun());
+        }
+
+        throw new RuntimeException(ctx.getText());
+    }
+
+    public Value evaluate(FunCallExprContext ctx) {
+        return eval.visit(ctx.funCall());
     }
 }
